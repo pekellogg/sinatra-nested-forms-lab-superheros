e3a2fd206a5a2f3368220ce1a8ee029e2065f4c7
Peyton <pekellogg@hotmail.com>
2022-03-16 14:42:01 -0700

    commit tree a3be86bde96c970f35501b5e71e2d7958e29ee7a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,24 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :'super_hero'
+    end
+
+    post '/teams' do
+        @team_name = params["team"]["name"]
+        @team_motto = params["team"]["motto"]
+        @team_members = params["team"]["members"]
+        @hero_names = []
+        @hero_powers = []
+        @hero_bios = []
+        @team_members.each do |member|
+            @hero_names << member["name"]
+            @hero_powers << member["power"]
+            @hero_bios << member["bio"]
+        end
+
+        erb :'team'
+    end
 
 end
